c5dcb28dd8da88bc8b14a34c3006715038198952
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2021-10-01 13:48:51 +0700

    npm module

--- app.js
+++ app.js
@@ -4,7 +4,16 @@
 
 // console.log(sum(2,3));
 
-const fs = require("fs");
+// ==================================================
+// const fs = require("fs");
 
-fs.copyFileSync("text.txt", "text2.txt");
-console.log("text.txt was copied to text2.txt");
+// fs.copyFileSync("text.txt", "text2.txt");
+// console.log("text.txt was copied to text2.txt");
+
+//==================================================
+const superheroes = require('superheroes');
+ 
+const heroName = superheroes.random();
+//=> 'Spider-Ham'
+
+console.log(heroName);
