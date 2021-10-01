b8aad6e3f24fb8594adde770df15d6933375e89f
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2021-10-01 13:47:48 +0700

    built-in module

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-function sum(a,b){
-    return a+b;
-}
+// function sum(a,b){
+//     return a+b;
+// }
 
-console.log(sum(2,3));
+// console.log(sum(2,3));
+
+const fs = require("fs");
+
+fs.copyFileSync("text.txt", "text2.txt");
+console.log("text.txt was copied to text2.txt");
